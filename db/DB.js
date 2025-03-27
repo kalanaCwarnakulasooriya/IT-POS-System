f4a8b4cfe404e781e012f0554f98ee5615400f2f
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-03-27 20:50:48 +0530

    Customer Section

---
+++ db/DB.js
@@ -0,0 +1,15 @@
+export let Customers = [
+  {
+    custId: "C01",
+    custName: "Kalana",
+    custAddress: "Anuradhapura",
+    custSalary: "95000",
+  },
+
+  {
+    custId: "C02",
+    custName: "Nishantha",
+    custAddress: "Matara",
+    custSalary: "22000",
+  },
+];
